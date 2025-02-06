feb4a3c506d0eb59bfd7085ba011e3d9006a536b
mailiap <mailiap@byu.edu>
2025-02-06 16:51:39 -0700

    test(authRouter)

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -2,25 +2,40 @@ const request = require('supertest');
 const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-let testUserAuthToken;
+let testUserAuthToken = null;
+let userID = null;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const badTestUser = { name: 'pizza diner', email: '', password: '' };
+  const wrongResponse = await request(app).post('/api/auth').send(badTestUser);
+  expect(wrongResponse.status).toBe(400);
+
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
-  expectValidJwt(testUserAuthToken);
 });
 
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
-  expectValidJwt(loginRes.body.token);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 
-  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
-  delete expectedUser.password;
-  expect(loginRes.body.user).toMatchObject(expectedUser);
+  const {password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(loginRes.body.user).toMatchObject(user);
+  expect(testUser.password).toBe(password);
+
+ userID = loginRes.body.user.id;
 });
 
-function expectValidJwt(potentialJwt) {
-  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-}
+test("update user",async ()=>{
+    
+  let emailNew = testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  let userNew = {email:`${emailNew}`, password:`${testUser.password}`};
+  const loginRes = await request(app).put(`/api/auth/${userID}`).set('Authorization', `Bearer ${testUserAuthToken}`).send(userNew)
+  expect(loginRes.body.email).toBe(emailNew);
+
+  const WrongIDRes = await request(app).put(`/api/auth/wrong`).set('Authorization', `Bearer ${testUserAuthToken}`).send(userNew)
+  expect(WrongIDRes.status).toBe(403);
+ })
+
+
