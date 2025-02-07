42254aae90346cd39f0bec9da5fd911ed94e8645
mailiap <mailiap@byu.edu>
2025-02-06 19:54:46 -0700

    80% coverage

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,7 +1,8 @@
 const request = require('supertest');
 const app = require('../service');
+
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-let testUserAuthToken;
+let testUserAuthToken = null;
 let userID = null;
 
 beforeAll(async () => {
@@ -9,6 +10,7 @@ beforeAll(async () => {
   const badTestUser = { name: 'pizza diner', email: '', password: '' };
   const wrongResponse = await request(app).post('/api/auth').send(badTestUser);
   expect(wrongResponse.status).toBe(400);
+
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
 });
@@ -17,49 +19,60 @@ test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
   expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
   const {password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
+  //obligated lint checker
   expect(testUser.password).toBe(password);
+
  userID = loginRes.body.user.id;
 });
 
 test("update user",async ()=>{
-    let emailNew = testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-    let userNew = {email:`${emailNew}`, password:`${testUser.password}`};
-    const loginRes = await request(app).put(`/api/auth/${userID}`).set('Authorization', `Bearer ${testUserAuthToken}`).send(userNew)
-    expect(loginRes.body.email).toBe(emailNew);
-    const WrongIDRes = await request(app).put(`/api/auth/wrong`).set('Authorization', `Bearer ${testUserAuthToken}`).send(userNew)
-    expect(WrongIDRes.status).toBe(403);
- });
+    
+  let emailNew = testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  let userNew = {email:`${emailNew}`, password:`${testUser.password}`};
+  const loginRes = await request(app).put(`/api/auth/${userID}`).set('Authorization', `Bearer ${testUserAuthToken}`).send(userNew)
+  expect(loginRes.body.email).toBe(emailNew);
 
-test('Unknown point', async () => {
+  const WrongIDRes = await request(app).put(`/api/auth/wrong`).set('Authorization', `Bearer ${testUserAuthToken}`).send(userNew)
+  expect(WrongIDRes.status).toBe(403);
+ })
+
+   //these are the first endpoints
+   test('Unknown point', async () => {
     const loginRes = await request(app).post('/api/auth/unknown').set('Authorization', `Bearer ${testUserAuthToken}`)
     .send(testUser); 
     expect(loginRes.status).toBe(404);
     expect(loginRes.body.message).toBe('unknown endpoint') 
+  });
+
+ test('logout', async () => {
+  const loginRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`)
+  .send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.text).toBe(`{"message":"logout successful"}`);
 });
 
-test('logout', async () => {
-    const loginRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`)
-    .send(testUser);
-    expect(loginRes.status).toBe(200);
-    expect(loginRes.text).toBe(`{"message":"logout successful"}`);
-});
-
-test('Docs', async () => {  
-    const loginRes = await request(app).get('/api/docs')
-    expect(loginRes.status).toBeDefined();
+ //these are the first endpoints
+ test('Docs', async () => {  
+  const loginRes = await request(app).get('/api/docs')
+  //console.log(loginRes.body)
+  expect(loginRes.status).toBeDefined();
 });
 
 test('Welcome to JWT', async () => {
-    const loginRes = await request(app).get('/')
-    expect(loginRes.status).toBe(200);
-    expect(loginRes.body.message).toBe('welcome to JWT Pizza')
+    
+  const loginRes = await request(app).get('/')
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.message).toBe('welcome to JWT Pizza')
 });
-  
+
 test('Unauthorized dealer', async () => {
-    let emailNew = testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-    let userNew = {email:`${emailNew}`, password:`${testUser.password}`};
-    const loginRes = await request(app).put(`/api/auth/${userID}`).set('Authorization', `Bearer sdsdwer`).send(userNew)
-    expect(loginRes.body.message).toBe("unauthorized");
+  let emialNew = testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  let userNew = {email:`${emialNew}`, password:`${testUser.password}`};
+  const loginRes = await request(app).put(`/api/auth/${userID}`).set('Authorization', `Bearer sdsdwer`).send(userNew)
+  expect(loginRes.body.message).toBe("unauthorized");
 });
+
+
--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -1,56 +1,74 @@
 const request = require("supertest")
 const app = require("../service.js")
 const { Role, DB } = require('../database/database.js');
+
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+
 let testUserAuthToken = null;
 let adminUserAuthToken = null;
-let newAdmin = null;
+let newAdminUser = null;
 
 function randomName() {
     return Math.random().toString(36).substring(2, 12);
-}
+  }
 
-async function createAdminUser() {
-    let user = { password: 'password123', roles: [{ role: Role.Admin }] };
+  async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
     user.name = randomName();
     user.email = user.name + '@admin.com';
+  
     await DB.addUser(user);
     return user;
-}  
+  }  
 
-beforeAll(async () => {
+  beforeAll(async () => {
     testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-    newAdmin = await createAdminUser();
-    let adminRegister = await request(app).put('/api/auth').send({"email":`${newAdmin.email}`, "password":"password123"});
-    adminUserAuthToken = adminRegister.body.token;
+    //login as admin
+    newAdminUser = await createAdminUser();
+
+    let adminRegisterRes = await request(app).put('/api/auth').send({"email":`${newAdminUser.email}`, "password":"toomanysecrets"});
+    adminUserAuthToken = adminRegisterRes.body.token;
+
+    //register new user and use his auth token probably overkill, definitely actually
     const registerRes = await request(app).post('/api/auth').send(testUser);
     testUserAuthToken = registerRes.body.token;
-});  
+  });  
 
-test("get pizza menu",async ()=>{
+  test("get pizza menu",async ()=>{
     const loginRes = await request(app).get(`/api/order/menu`);
     expect(loginRes.body).toBeDefined();
-});
+  })
 
-test("Unsuccessfully add item to menu",async ()=>{
-    let newItem = { "title":"Student", "description": "Everything on the burger please", "image":"nunya.png", "price": 0.0001 }
-    const menuResponse = await request(app).put("/api/order/menu").set('Authorization', `Bearer ${testUserAuthToken}`).send(newItem);
+  test("Unsuccessfully Add an item to menu",async ()=>{
+    let newItemOnMenu = { "title":"Student", "description": "Everything on the burger please", "image":"nunya.png", "price": 0.0001 }
+    const menuResponse = await request(app).put("/api/order/menu").set('Authorization', `Bearer ${testUserAuthToken}`).send(newItemOnMenu);
     expect(menuResponse.statusCode).toBe(403)
-});
+   })
 
-test("Successfully add item to the menu ",async ()=>{
-    let newItem = { "title":"Student", "description": "Everything on the burger please", "image":"nunya.png", "price": 0.0001 }
-    const correctMenuResponse = await request(app).put("/api/order/menu").set('Authorization', `Bearer ${adminUserAuthToken}`).send(newItem);
-    expect(correctMenuResponse.statusCode).toBe(200);
-});
+   test("Successfully add an item to the menu ",async ()=>{
 
-test('Create an order', async () => {
+    //current admin user is not working so using another admin account to sign in
+
+    let newItemOnMenu = { "title":"Student", "description": "Everything on the burger please", "image":"nunya.png", "price": 0.0001 }
+       const correctMenuResponse = await request(app).put("/api/order/menu").set('Authorization', `Bearer ${adminUserAuthToken}`).send(newItemOnMenu);
+ 
+       expect(correctMenuResponse.statusCode).toBe(200);
+   }) 
+
+   test('Create an order', async () => {
+   
     let newOrder = {"franchiseId": 2, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]}
     const loginRes = await request(app).post('/api/order').set('Authorization', `Bearer ${testUserAuthToken}`).send(newOrder)
+
     expect(loginRes.body.order.franchiseId).toBe(2)
+
     let badOrder = {"franchiseId": 2, "storeId":1,"Nah":"bad","this one has to be bad":null, "items":[{ "menuId": null, "description": "Veggie", "price": 0.05 }]}
     const badRes = await request(app).post('/api/order').set('Authorization', `Bearer BADAUTH?`).send(newOrder)
+
     expect(badRes.status).toBe(401);
+
     const badRes500 = await request(app).post('/api/order').set('Authorization', `Bearer ${testUserAuthToken}`).send(badOrder);
+
     expect(badRes500.statusCode).toBe(500);
-});
+    
+  });
